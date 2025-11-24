db1af1fcb4d0a453501f654117cf5d69154477fc
Euro IT <contact@euroitechnology.com>
2025-11-24 07:19:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3993279439-e23e2",
+  "appId": "1:967574440406:web:abd4fefa3ee08d5257bc77",
+  "apiKey": "AIzaSyAVqsEEdoW1Mcp2ql50EakiSSk-4QK3WBs",
+  "authDomain": "studio-3993279439-e23e2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "967574440406"
+};
